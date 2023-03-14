720a9a892e17d7f903e07bc1a3187a8c9f46513f
AlexLojko <a_l_g@tut.by>
2023-03-14 23:30:36 +0300

    add AlphabetPosition

--- codewar/AlphabetPosition.py
+++ codewar/AlphabetPosition.py
@@ -6,24 +6,27 @@ alphabet_position("The sunset sets at twelve o' clock.") "20 8 5 19 21 14 19 5 2
 text = "The sunset sets at twelve o' clock."
 
 
-def alphabet_position(text):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    listText = list(text.lower())
-    newtext = ''
-    while not text.isalpha():
-        for i in listText:
-            if not i in list(alphabet):
-                listText.remove(i)
-        text = ''.join(listText)
-        if not text:
-            return newtext
+# def alphabet_position(text):
+#     alphabet = "abcdefghijklmnopqrstuvwxyz"
+#     listText = list(text.lower())
+#     newtext = ''
+#     while not text.isalpha():
+#         for i in listText:
+#             if not i in list(alphabet):
+#                 listText.remove(i)
+#         text = ''.join(listText)
+#         if not text:
+#             return newtext
+#
+#     for i in listText[:-1]:
+#         newtext += str(alphabet.find(i) + 1) + ' '
+#     newtext += str(alphabet.find(listText[-1])+1)
+#     return newtext
 
-    for i in listText[:-1]:
-        newtext += str(alphabet.find(i) + 1) + ' '
-    newtext += str(alphabet.find(listText[-1])+1)
-    return newtext
+s = "The sunset sets at twelve o' clock."
 
-
+def alphabet_position(s):
+  return ' '.join(str(ord(c)-ord('a')+1) for c in s.lower() if c.isalpha())
 
 print(alphabet_position(text))
-
+print(alphabet_position(s))
