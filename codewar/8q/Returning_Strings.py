f18a87340ed4e525e142aa08dffdeea039dae830
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2023-06-07 15:55:46 +0300

    add 8q

---
+++ codewar/8q/Returning_Strings.py
@@ -0,0 +1,4 @@
+# Создайте функцию, которая будет возвращать оператор приветствия, использующий ввод;
+# ваша программа должна вернуться, "Hello, <name> how are you doing today?".
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
