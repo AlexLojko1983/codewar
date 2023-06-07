24cbee7b49416676b580e17d0b5fb95da07dbc41
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2023-06-07 14:41:50 +0300

    add 6q

---
+++ codewar/6/Create Phone Number.py
@@ -0,0 +1,11 @@
+def create_phone_number(n):
+    numbers = '1234567890'
+    str_n = ''.join([i for i in str(n) if i in numbers])
+    return f'({str_n[:3]}) {str_n[3:6]}-{str_n[6:]}'
+
+
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(create_phone_number(list_1))
+
+# def create_phone_number(n):
+# 	return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
