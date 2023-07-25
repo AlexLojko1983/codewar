2d18e212132323b6bf3558a65da0d8a3bd9f8f6b
AlexLojko <a_l_g@tut.by>
2023-07-25 19:52:35 +0300

    3q

--- codewar/3q/Prime_counting.py
+++ codewar/3q/Prime_counting.py
@@ -1,17 +1,19 @@
-def count_primes_less_than(n: int) -> int:
-    num = list(range(2, n + 1))
-    list_1 = []
-    p = 0
-    j = 0
-    while p <= n ** 0.5:
-        p = num[j]
-        list_1.append(p)
-        for i in range(p, (n + 1) // p + 1):
-            # num.remove(p)
-            if i * p in num:
-                num.remove(i * p)
-        num.remove(p)
-    return len(list_1)
+def count_primes_less_than(N: int) -> int:
+    primes = [True] * (N+1)
+    primes[0] = primes[1] = False
 
+    p = 2
+    while p * p <= N:
+        if primes[p]:
+            for i in range(p * p, N+1, p):
+                primes[i] = False
+        p += 1
+
+    prime_numbers = []
+    for i in range(2, N+1):
+        if primes[i]:
+            prime_numbers.append(i)
+
+    return len(prime_numbers)
 
 print(count_primes_less_than(340000))
