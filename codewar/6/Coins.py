da8ec2da5db5a5a8e6de9c7b326625ba52006224
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2023-08-14 14:09:30 +0300

    add 6q

--- codewar/6/Coins.py
+++ codewar/6/Coins.py
@@ -1,10 +1,23 @@
-def coins(coin1,coin2):
-    if coin1%2 and coin2%2:
+def coins(coin1, coin2):
+    if coin1 % 2 and coin2 % 2:
         return -1
-    elif not coin1%2 and not coin2%2:
+    elif not coin1 % 2 and not coin2 % 2:
         return -1
     else:
-        if coin1 > coin2:
-            return coin2-1
-        else: return coin1-1
-print(coins(2,3))
+        if coin1<coin2:
+            f = coin1 - 1
+        else:
+            f= coin2 -1
+        a= list(range(coin1*10 + coin2*10))
+        for n in range(31):
+            for l in range(31):
+                c=coin1*n + coin2*l
+                if a.count(c)!=0:
+                    a.remove(c)
+        print(a)
+        f= max(a)
+
+        return f
+
+
+print(coins(11, 18))
