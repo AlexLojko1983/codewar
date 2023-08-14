85e160e45e8681273dc8abba3200d02a246a757b
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2023-08-14 16:48:42 +0300

    add 6q

--- codewar/6/Coins.py
+++ codewar/6/Coins.py
@@ -1,23 +1,27 @@
 def coins(coin1, coin2):
-    if coin1 % 2 and coin2 % 2:
+    if not coin1%2 and not coin2%2:
         return -1
-    elif not coin1 % 2 and not coin2 % 2:
+    elif coin1<coin2:
+        f = coin1 - 1
+    else:
+        f= coin2 -1
+    a= set(range(coin1*50 + coin2*50))
+    # print(a)
+    c = list()
+    for n in range(100):
+        for l in range(100):
+            c.append(coin1*n + coin2*l)
+            # print(c)
+            b= a.difference(set(c))
+
+    print(b)
+    print(max(a))
+    if len(b)==0:
+        return -1
+    elif len(b)>100:
         return -1
     else:
-        if coin1<coin2:
-            f = coin1 - 1
-        else:
-            f= coin2 -1
-        a= list(range(coin1*10 + coin2*10))
-        for n in range(31):
-            for l in range(31):
-                c=coin1*n + coin2*l
-                if a.count(c)!=0:
-                    a.remove(c)
-        print(a)
-        f= max(a)
-
+        f= max(b)
         return f
 
-
-print(coins(11, 18))
+print(coins(51, 24))
