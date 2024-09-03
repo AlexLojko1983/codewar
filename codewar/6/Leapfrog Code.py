124f9007a7cb45d726f6b73188a358cfe7a2e893
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-09-03 11:13:55 +0300

    add 6q test

--- codewar/6/Leapfrog Code.py
+++ codewar/6/Leapfrog Code.py
@@ -3,12 +3,20 @@
 # • If the element is a string, append its reversed copy to its end like that "string" becomes "stringgnirts"
 # • If the element is a bool, simply reverse it. True becomes False and False becomes True
 # • Reverse the list
-def change_list(arr):
-    for i in range(len(arr)):
-        if type(arr[i]) is int: arr[i] = arr[i] ** 2
-        elif type(arr[i]) is bool: arr[i] =  not arr[i]
-        elif type(arr[i]) is str: arr[i] =  arr[i] + ''.join(arr[i][::-1])
-        i=i+1
-    return arr
+def change_list(*args):
+    if not args:
+        arr = list(input())[::2]
+        return ''.join(arr)
+    else:
+        arr = args[0]
+        for i in range(len(arr)):
+            if type(arr[i]) is int: arr[i] = arr[i] ** 2
+            elif type(arr[i]) is bool: arr[i] = not arr[i]
+            elif type(arr[i]) is str: arr[i] = arr[i] + ''.join(arr[i][::-1])
+            i = i+1
+        arr = arr[::-1]
+        return arr
 
-print(change_list([5, 'fds', True,[1, 2, 3]]))
+
+print(change_list([3, 5, "hello", False, True, 9, "codewars", True]))
+# print(change_list())
