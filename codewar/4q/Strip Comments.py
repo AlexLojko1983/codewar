ad85f8befff04875920de873391ef5237250004b
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-09-27 12:23:08 +0300

    create 4q

---
+++ codewar/4q/Strip Comments.py
@@ -0,0 +1,7 @@
+'''Complete the solution so that it strips all text that follows any of a set of comment markers passed in.
+Any whitespace at the end of the line should also be stripped out.
+result = strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+# result should == "apples, pears\ngrapes\nbananas"
+'''
+def strip_comments(strng, markers):
+    pass
