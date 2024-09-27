cc0d29ae4a50312f0692934087329dda684ff063
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-09-27 15:24:08 +0300

    create 4q ++

--- codewar/4q/Strip Comments.py
+++ codewar/4q/Strip Comments.py
@@ -3,5 +3,15 @@ Any whitespace at the end of the line should also be stripped out.
 result = strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
 # result should == "apples, pears\ngrapes\nbananas"
 '''
-def strip_comments(strng, markers):
-    pass
+
+
+def strip_comments(strng: str, markers: list):
+    should = ""
+    # qwe = ''.join(strng.split(" ")).split("\n")
+    qwe = strng.split("\n")
+    print(qwe)
+    for i in qwe:
+        if i in markers:
+            should += i[:i.find('#')] + "\n"
+    return should
+print(strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
