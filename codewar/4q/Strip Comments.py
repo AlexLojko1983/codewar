d3f4ac76e73d2c3081df252bae15c7784c666425
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-09-30 10:40:10 +0300

    add 4q

--- codewar/4q/Strip Comments.py
+++ codewar/4q/Strip Comments.py
@@ -4,14 +4,15 @@ result = strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", 
 # result should == "apples, pears\ngrapes\nbananas"
 '''
 
+import re
+
 
 def strip_comments(strng: str, markers: list):
-    should = ""
-    # qwe = ''.join(strng.split(" ")).split("\n")
-    qwe = strng.split("\n")
-    print(qwe)
-    for i in qwe:
-        if i in markers:
-            should += i[:i.find('#')] + "\n"
-    return should
-print(strip_comments("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
+    escaped_markers = [re.escape(marker) for marker in markers]
+    markers = '|'.join(escaped_markers)
+    strng = strng.split('\n')
+    trimmed_lines = [re.split(markers, line)[0].rstrip() if markers else line for line in strng]
+    return '\n'.join(trimmed_lines)
+
+
+print(strip_comments('a #b\nc\nd $e f g', ['#', '$']))
