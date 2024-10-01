49ba90768bc5c40cc9332ab3463d936809e08da6
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-01 09:29:27 +0300

    create 4q

---
+++ codewar/4q/Explosive Sum.py
@@ -0,0 +1,11 @@
+'''
+How many ways can you make the sum of a number?
+In number theory and combinatorics, a partition of a positive integer n,
+also called an integer partition, is a way of writing n as a sum of positive integers.
+Two sums that differ only in the order of their summands are considered the same partition.
+If order matters, the sum becomes a composition. For example, 4 can be partitioned in five distinct ways:
+'''
+
+number = 5
+number_list = [1]*number
+print (number_list)
