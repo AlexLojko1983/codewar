75661d3d18b6a66a9c8ea71325b0492ad462d8df
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-03 16:10:41 +0300

    add 6q

--- codewar/6/Are they the same.py
+++ codewar/6/Are they the same.py
@@ -7,4 +7,10 @@ that checks whether the two arrays have the "same" elements, with the same multi
 
 
 def comp(array1, array2):
-# your code
+    if array2 is None or array1 is None: return False
+    return sorted([_**2 for _ in array1]) == sorted(array2)
+
+a1 = [121, 144, 19, 161, 19, 144, 19, 11]
+a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+print(comp(a1, a2))
