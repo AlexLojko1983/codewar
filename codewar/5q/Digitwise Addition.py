1bbd58b6bf829c024f4bb1bd491dcf3be0e19226
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-04 15:07:27 +0300

    add 5q++

--- codewar/5q/Digitwise Addition.py
+++ codewar/5q/Digitwise Addition.py
@@ -18,28 +18,35 @@ it adds 1 to every digit of that number. If the digit is a 9, we replace it with
 #     s = digitwise(n)
 #     for i in range(k-1):
 #         s = digitwisestr(s)
-#     return len(str(s))
+#     return (s)
+
+
 
 def digitwise_addition(n, k):
-    lst = [_ for _ in str(n)]
-    count = len(lst)
-    for i in range(len(lst)):
-        delta = 10 - int(lst[i])
-        '''
-        1. Если delta = k, надо добавить 1
-        2. Если delta < k и k-delta меньше 10, +1
-        3. Если delta < k и k-delta больше 10, +//10
-        4. Если остаток от деления больше или = дельте, +1
-        '''
-        if delta <= k and k - delta < 10:
-            count += 1
-        elif delta < k and k - delta >= 10:
-            count += 1 + (k - delta) // 10
-        if k - delta >= 10 and delta <= (k - delta) % 10:
-            count += 1
-    return count
+    import sys
+    sys.set_int_max_str_digits(10 ** 6)
 
+    def add_one_to_digits(x):
+        result = 0
+        multiplier = 1
+        while x > 0:
+            digit = x % 10
+            x //= 10
+            if digit == 9:
+                result += 10 * multiplier
+                multiplier *= 100
+            else:
+                result += (digit + 1) * multiplier
+                multiplier *= 10
+        return result
 
-print(digitwise_addition(433365377, 14))
+    for _ in range(k):
+        n = add_one_to_digits(n)
 
-# n=433365377 and k=14
+    return len(str(n))
+#
+#
+print(digitwise_addition(143948553, 16))
+
+# n=143948553 and k=16; 35 should equal 32.
+9887878721101098732213221
