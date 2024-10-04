2014e423bdfbc6854aae49d70c92662f3163ac93
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-04 12:41:02 +0300

    create 5q++

--- codewar/5q/Digitwise Addition.py
+++ codewar/5q/Digitwise Addition.py
@@ -4,20 +4,42 @@ it adds 1 to every digit of that number. If the digit is a 9, we replace it with
 '''
 
 
-def digitwise(n):
-    lst_end = [str(int(x)+1) for x in [_ for _ in str(n)]]
-    return ''.join(lst_end)
-
-def digitwisestr(s):
-    lst_end = [str(int(x)+1) for x in s]
-    return ''.join(lst_end)
-
+# def digitwise(n):
+#     lst_end = [str(int(x)+1) for x in [_ for _ in str(n)]]
+#     return ''.join(lst_end)
+#
+# def digitwisestr(s):
+#     lst_end = [str(int(x)+1) for x in s]
+#     return ''.join(lst_end)
+#
+#
+# def digitwise_addition(n, k):
+#     if k == 1: return len(digitwise(n))
+#     s = digitwise(n)
+#     for i in range(k-1):
+#         s = digitwisestr(s)
+#     return len(str(s))
 
 def digitwise_addition(n, k):
-    if k == 1:  return len(digitwise(n))
-    s = digitwise(n)
-    for i in range(k-1):
-        s = digitwisestr(s)
-    return len(str(s))
+    lst = [_ for _ in str(n)]
+    count = len(lst)
+    for i in range(len(lst)):
+        delta = 10 - int(lst[i])
+        '''
+        1. Если delta = k, надо добавить 1
+        2. Если delta < k и k-delta меньше 10, +1
+        3. Если delta < k и k-delta больше 10, +//10
+        4. Если остаток от деления больше или = дельте, +1
+        '''
+        if delta <= k and k - delta < 10:
+            count += 1
+        elif delta < k and k - delta >= 10:
+            count += 1 + (k - delta) // 10
+        if k - delta >= 10 and delta <= (k - delta) % 10:
+            count += 1
+    return count
 
-print(digitwise_addition(1399,2))
+
+print(digitwise_addition(433365377, 14))
+
+# n=433365377 and k=14
