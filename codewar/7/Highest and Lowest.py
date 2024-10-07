8c47c1b4f5fc809c9ab9f9c0e5ab21681a5ea85a
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-07 11:32:01 +0300

    add 7q

---
+++ codewar/7/Highest and Lowest.py
@@ -0,0 +1,19 @@
+'''
+In this little assignment you are given a string of space separated numbers,
+and have to return the highest and lowest number.
+'''
+
+
+def high_and_low(numbers: str):
+    numbers = numbers.split(' ')
+    max = int(numbers[0])
+    min = int(numbers[0])
+
+    for digit in numbers:
+        if int(digit) > max:
+            max = int(digit)
+        if int(digit) < min:
+            min = int(digit)
+    return f'{max} {min}'.format(max, min)
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4"))
