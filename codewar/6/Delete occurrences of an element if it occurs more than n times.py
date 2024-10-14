919f5f7589f302cf06135384f60017753ebf755c
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-14 08:47:31 +0300

    add 6q+

--- codewar/6/Delete occurrences of an element if it occurs more than n times.py
+++ codewar/6/Delete occurrences of an element if it occurs more than n times.py
@@ -16,3 +16,11 @@ def delete_nth(order, max_e):
 
 
 print(delete_nth([1, 1, 3, 3, 7, 2, 2, 2, 2], 3))
+
+'''
+def delete_nth(order,max_e):
+    ans = []
+    for o in order:
+        if ans.count(o) < max_e: ans.append(o)
+    return ans
+'''
