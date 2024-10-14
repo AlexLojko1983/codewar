351ccde8122a5b5c24da64d1858b5a044b64354d
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-14 08:53:18 +0300

    create 7q

---
+++ codewar/7/Sum of odd numbers.py
@@ -0,0 +1,8 @@
+'''
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+1 -->  1
+2 --> 3 + 5 = 8
+'''
+
+def row_sum_odd_numbers(n):
+    #your code here
