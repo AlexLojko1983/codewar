a569d57d6550acdb148334ec61e674af0827b183
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-14 12:36:33 +0300

    add 7q

--- codewar/7/Sum of odd numbers.py
+++ codewar/7/Sum of odd numbers.py
@@ -4,5 +4,11 @@ Calculate the sum of the numbers in the nth row of this triangle (starting at in
 2 --> 3 + 5 = 8
 '''
 
+
 def row_sum_odd_numbers(n):
-    #your code here
+    list_of_numbers = [i for i in range(1, n + n ** 2) if i % 2 != 0]
+    list_of_numbers.reverse()
+    return sum(list_of_numbers[:n])
+
+
+print(row_sum_odd_numbers(13))
