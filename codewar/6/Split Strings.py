9a8f9a23e68b916fde4937abaade78822e89dc48
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-17 08:55:17 +0300

    create 6q

--- codewar/6/Split Strings.py
+++ codewar/6/Split Strings.py
@@ -26,3 +26,13 @@ def solution(s):
     return res[1:]
 
 print(solution('abcdefj'))
+
+'''
+def solution(s):
+    result = []
+    if len(s) % 2:
+        s += '_'
+    for i in range(0, len(s), 2):
+        result.append(s[i:i+2])
+    return result
+'''
