bd1cc915c6de095aba96af4990beb4c8ce4c0481
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-17 08:52:41 +0300

    add 6q+

--- codewar/6/Split Strings.py
+++ codewar/6/Split Strings.py
@@ -11,11 +11,18 @@ Examples:
 
 def solution(s):
     if len(s) % 2 != 0: s = s + '_'
-    result = s.split()
-    for i in range(len(s)):
-        if i%2 == 0:
-            result.insert(i,' ')
-    return result
+    s_split = [x for x in s]
+    result = []
+    for i in range(len(s_split)-1, -1,-1):
+        if i % 2 == 0:
+            result.append(s_split[i])
+            result.append(' ')
+        else:
+            result.append(s_split[i])
 
+    result.reverse()
+    res = ''.join(result).rstrip().split(' ')
+
+    return res[1:]
 
 print(solution('abcdefj'))
