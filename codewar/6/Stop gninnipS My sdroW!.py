12785d26ab35c47dc60370cac31d4db1426464b1
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-18 08:38:58 +0300

    add 6q+

--- codewar/6/Stop gninnipS My sdroW!.py
+++ codewar/6/Stop gninnipS My sdroW!.py
@@ -15,3 +15,9 @@ def spin_words(sentence):
 
 sentence = "Hey fellow warriors"
 print(spin_words(sentence))
+
+'''
+def spin_words(sentence):
+    # Your code goes here
+    return " ".join([x[::-1] if len(x) >= 5 else x for x in sentence.split(" ")])
+'''
