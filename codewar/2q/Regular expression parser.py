3790e1f6e9148044b319ca0eb33306c80c22840d
Alexander <123839012+AlexLojko1983@users.noreply.github.com>
2024-10-21 15:37:35 +0300

    create 2q+

--- codewar/2q/Regular expression parser.py
+++ codewar/2q/Regular expression parser.py
@@ -33,23 +33,24 @@ Feel free to use any parser combinator libraries available on codewars, or imple
 
 '''
 
-from preloaded import Any, Normal, Or, Str, ZeroOrMore
+# from preloaded import Any, Normal, Or, Str, ZeroOrMore
 
 # preloaded defines the following:
 
-# class RegExp:
-#     def __init__(self, *args):
-#         self.args = args
-#     def __repr__(self):
-#         args = ", ".join(map(repr, self.args))
-#         return f"{self.__class__.__name__}({args})"
-#     def __eq__(self, other):
-#         return type(self) is type(other) and self.args == other.args
-# class Any(RegExp): pass
-# class Normal(RegExp): pass
-# class Or(RegExp): pass
-# class Str(RegExp): pass
-# class ZeroOrMore(RegExp): pass
+class RegExp:
+    def __init__(self, *args):
+        self.args = args
+    def __repr__(self):
+        args = ", ".join(map(repr, self.args))
+        return f"{self.__class__.__name__}({args})"
+    def __eq__(self, other):
+        return type(self) is type(other) and self.args == other.args
+class Any(RegExp): pass
+class Normal(RegExp):
+    pass
+class Or(RegExp): pass
+class Str(RegExp): pass
+class ZeroOrMore(RegExp): pass
 
 # Your task is to build an AST using those nodes.
 # See sample tests or test output for examples of usage.
